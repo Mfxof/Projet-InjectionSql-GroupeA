bf4678b7181176dd1c7d21db2ff27e11e71c0ab1
Mfxof <matysfreyermuth@gmail.com>
2023-09-25 17:22:12 +0200

    V 1.3.9 Formulaire

---
+++ Site/Concept copy.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic Frites - Contact</title>
+
+<?php
+    include('assets/php/nav.php');
+    ?>
+
+<body>
+<!-- ***** Our Classes Start ***** -->
+<section class="section" id="our-classes">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Nos <em>Cours</em></h2>
+                        <img src="assets/images/line-dec.png" alt="">
+                        <p>Nunc urna sem, laoreet ut metus id, aliquet consequat magna. Sed viverra ipsum dolor, ultricies fermentum massa consequat eu.</p>
+                    </div>
+                </div>
+            </div>
+            <div class="row" id="tabs">
+  <div class="col-lg-4">
+    <ul>
+      <li><a href='#tabs-1'><img src="assets/images/tabs-first-icon.png" alt="">Première leçon</a></li>
+      <li><a href='#tabs-2'><img src="assets/images/tabs-first-icon.png" alt="">Deuxième leçon</a></li>
+      <li><a href='#tabs-3'><img src="assets/images/tabs-first-icon.png" alt="">Troisième leçon</a></li>
+      <li><a href='#tabs-4'><img src="assets/images/tabs-first-icon.png" alt="">Quatrième leçon</a></li>
+      <div class="main-rounded-button"><a href="#">Voir toutes les leçons</a></div>
+    </ul>
+  </div>
+  <div class="col-lg-8">
+    <section class='tabs-content'>
+      <article id='tabs-1'>
+        <img src="assets/images/training-image-01.jpg" alt="Première Classe">
+        <h4>Première Classe de Formation</h4>
+        <p>Phasellus convallis mauris sed elementum vulputate. Donec posuere leo sed dui eleifend hendrerit. Sed suscipit suscipit erat, sed vehicula ligula. Aliquam ut sem fermentum sem tincidunt lacinia gravida aliquam nunc. Morbi quis erat imperdiet, molestie nunc ut, accumsan diam.</p>
+        <div class="main-button">
+          <a href="#">Voir l'emploi du temps</a>
+        </div>
+      </article>
+      <article id='tabs-2'>
+        <img src="assets/images/training-image-02.jpg" alt="Deuxième Formation">
+        <h4>Deuxième Classe de Formation</h4>
+        <p>Integer dapibus, est vel dapibus mattis, sem mauris luctus leo, ac pulvinar quam tortor a velit. Praesent ultrices erat ante, in ultricies augue ultricies faucibus. Nam tellus nibh, ullamcorper at mattis non, rhoncus sed massa. Cras quis pulvinar eros. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+        <div class="main-button">
+          <a href="#">Voir l'emploi du temps</a>
+        </div>
+      </article>
+      <article id='tabs-3'>
+        <img src="assets/images/training-image-03.jpg" alt="Troisième Classe">
+        <h4>Troisième Classe de Formation</h4>
+        <p>Fusce laoreet malesuada rhoncus. Donec ultricies diam tortor, id auctor neque posuere sit amet. Aliquam pharetra, augue vel cursus porta, nisi tortor vulputate sapien, id scelerisque felis magna id felis. Proin neque metus, pellentesque pharetra semper vel, accumsan a neque.</p>
+        <div class="main-button">
+          <a href="#">Voir l'emploi du temps</a>
+        </div>
+      </article>
+      <article id='tabs-4'>
+        <img src="assets/images/training-image-04.jpg" alt="Quatrième Formation">
+        <h4>Quatrième Classe de Formation</h4>
+        <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultrices elementum odio ac tempus. Etiam eleifend orci lectus, eget venenatis ipsum commodo et.</p>
+        <div class="main-button">
+          <a href="#">Voir l'emploi du temps</a>
+        </div>
+      </article>
+    </section>
+  </div>
+</div>
+</div>
+</section>
+<!-- ***** Nos Cours Finissent Ici ***** -->
+</body>
+</html>
+
+<?php
+  include('assets/php/footer.php');
+?>
