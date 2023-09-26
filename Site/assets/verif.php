4a5212e9b7db11e8638d0eeb560bb83110a6ee44
Mfxof <matysfreyermuth@gmail.com>
2023-09-26 16:38:02 +0200

    Ajout du form v 1.4.3

--- Site/assets/verif.php
+++ Site/assets/verif.php
@@ -1,12 +1,28 @@
 <?php
-// Récupérer les informations du formulaire
-$email = $_POST['email'];
-$password = $_POST['password'];
+$host = "localhost";
+$database = "basic_frites";
+$username = "root";
+$password = "admin";
 
-// Vérifier l'email et le mot de passe
-if ($email == 'xiode.gam@gmail.com' && $password == 'admin') {
-    echo "Connexion réussie !";
-} else {
-    echo "Email ou mot de passe incorrect.";
+$user = $_POST['username'];
+$pass = $_POST['password'];
+
+$mysqli = new mysqli($host, $username, $password, $database);
+
+if ($mysqli->connect_error) {
+    die("La connexion à la base de données a échoué : " . $mysqli->connect_error);
 }
+
+$user = $mysqli->real_escape_string($user);
+$pass = $mysqli->real_escape_string($pass);
+
+$query = "SELECT * FROM membre WHERE Login='$user' AND Psw='$pass'";
+$result = $mysqli->query($query);
+
+if ($result->num_rows == 1) {
+    echo "Authentification réussie. Vous pouvez accéder à votre compte.";
+} else {
+    echo "Erreur : Nom d'utilisateur ou mot de passe incorrect.";
+}
+$mysqli->close();
 ?>
