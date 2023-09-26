1a7fd2aa94efb89d9b5849a9a5074478e1bfc42f
ozn3rol <loris067@hotmail.fr>
2023-09-26 16:55:06 +0200

    création page profil

---
+++ Site/profil.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Basic Frites - Contact</title>
+
+<?php
+    include('assets/php/nav.php');
+    ?>
+
+<body>
+    <!-- ***** Section Profil Début ***** -->
+    <section class="section" id="features">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 offset-lg-3">
+                    <div class="section-heading">
+                        <h2>Bienvenue sur <em>votre Profil</em></h2>
+                        <img src="assets/images/line-dec.png" alt="vagues">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="Premier">
+                            </div>
+                            <div class="right-content">
+                            <label for="username">Nom d'utilisateur</label>
+
+                            </div>
+                        </li>
+                        <br>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="deuxième">
+                            </div>
+                            <div class="right-content">
+                            <label for="username">Adresse Mail</label>
+
+                            </div>
+                        </li>
+                        <br>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="troisième">
+                            </div>
+                            <div class="right-content">
+                            <label for="username">Numéro de téléphone</label>
+
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-6">
+                    <ul class="features-items">
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="quatrième">
+                            </div>
+                            <div class="right-content">
+                            <label for="username">Mot de Passe</label>
+
+                            </div>
+                        </li>
+                        <br>
+                        <li class="feature-item">
+                            <div class="left-icon">
+                                <img src="assets/images/features-first-icon.png" alt="cinquième">
+                            </div>
+                            <div class="right-content">
+                            <label for="username">Formation Inscrit</label>
+
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ***** Section Profil Fin ***** -->
+</body>
+</html>
+
+<?php
+    include('assets/php/footer.php');
+    ?>
